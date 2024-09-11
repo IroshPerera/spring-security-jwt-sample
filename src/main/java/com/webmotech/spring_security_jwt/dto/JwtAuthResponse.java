3e722bb4570ea7c60c29cda02824a2aa7e7d0557
Irosh Perera <irosh@webmotech.com>
2024-09-11 14:15:19 +0530

    implement dtos

---
+++ src/main/java/com/webmotech/spring_security_jwt/dto/JwtAuthResponse.java
@@ -0,0 +1,16 @@
+package com.webmotech.spring_security_jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class JwtAuthResponse {
+    private String token;
+    private UserDTO userDTO;
+
+}
